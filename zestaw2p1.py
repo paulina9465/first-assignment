43cac2a7caf52ab616d8dbed2a7fb96400f89929
Paulina Pielacinska <pielacinskapaulina@gmail.com>
2020-07-04 15:53:59 +0200

    Zestaw 2 punkt 2

--- zestaw2p1.py
+++ zestaw2p1.py
@@ -1,4 +1,6 @@
 import pandas
+from sklearn.preprocessing import OneHotEncoder
+import sklearn
 import matplotlib.pyplot
 import matplotlib.pyplot
 import seaborn
@@ -8,9 +10,15 @@ from scipy import stats
 
 #zmienna zależna (y):  WorkWeekHrs , niezalezne zmienne (x1, x2): CompTotal, CodeRevHrs
 file = pandas.read_csv('survey_results_public.csv', usecols=['YearsCode', 'Age1stCode', 'CompTotal', 'WorkWeekHrs',
-                       'CodeRevHrs'])
+                       'CodeRevHrs', 'Hobbyist', 'MainBranch'])
 
 file.dropna(inplace=True)
-
+file.replace(to_replace={'Yes': '1', 'No': '0'}, inplace=True)
+print(file)
 print(file.corr())
-print(file.describe())
+
+
+one_hot = pandas.get_dummies(file['MainBranch'])
+file = file.drop('MainBranch',axis = 1)
+file = file.join(one_hot)
+print(file)
